006f706f6f12156802d3981ea02f1eb1fa2da223
welligton-solutionSant <welligtoncos@gmail.com>
2024-10-31 09:13:41 -0300

    feat: projeto base

---
+++ Infra/config/ContextBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Entities.Context
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options) { }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+            }
+        }
+
+        private string ObterStringConexao()
+        {
+            return "Data Source=DESKTOP-4LE6SQB;Initial Catalog=Acaiteria;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
